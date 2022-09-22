181c135b860e153f1f4a22c1ae66401e6b91f07a
Alex <alexandertaylor315@gmail.com>
2022-09-22 00:35:04 -0400

    created welcome method in Game class

--- game.py
+++ game.py
@@ -1,5 +1,25 @@
 
+from time import pthread_getcpuclockid
+
+
 class Game():
 
     def __init__(self):
         pass
+
+    def display_welcome(self):
+        print("Welcome to Rock Paper Scissors Lizard Spock")
+
+        print("Each game with be best 2 out of 3")
+        print("Use number keys to make your selection")
+
+        print("Scissors cuts paper")
+        print("Paper covers rock")
+        print("Rock crushes lizard")
+        print("Lizard poisons Spock")
+        print("Spock smashes scissors")
+        print("Scissors decapitates lizard")
+        print("Lizard eats paper")
+        print("Paper disproves Spock")
+        print("Spock vaporizes rock")
+        print("Rock crushes scissors")
